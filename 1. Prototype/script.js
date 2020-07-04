ff1292df506b543975bddd0243e7f9f5fbce38dc
abuabdirohman4 <kakaabu354@yahoo.com>
2020-07-04 14:21:59 +0700

    Materi 1.3 Prototype

--- 1. Prototype/script.js
+++ 1. Prototype/script.js
@@ -45,41 +45,40 @@
 // let abu = Mahasiwa("Abu Abdirohman", 10);
 // let hanan = Mahasiwa("Azati Hanani", 10);
 
-// * Penyelesaian Problem 1
+// // * Penyelesaian Problem 1
+// const methodMahasiwa = {
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//     console.log(`Halo ${this.nama}, selamat makan!`);
+//   },
 
-const methodMahasiwa = {
-  makan: function (porsi) {
-    this.energi += porsi;
-    console.log(`Halo ${this.nama}, selamat makan!`);
-  },
+//   main: function (jam) {
+//     this.energi -= jam;
+//     console.log(`Halo ${this.nama}, selamat bermain!`);
+//   },
 
-  main: function (jam) {
-    this.energi -= jam;
-    console.log(`Halo ${this.nama}, selamat bermain!`);
-  },
+//   tidur: function (jam) {
+//     this.energi += jam * 2;
+//     console.log(`Halo ${this.nama}, selamat tidur`);
+//   },
+// };
 
-  tidur: function (jam) {
-    this.energi += jam * 2;
-    console.log(`Halo ${this.nama}, selamat tidur`);
-  },
-};
+// function Mahasiswa(nama, energi) {
+//   // * Penggunaan Object.create agar tidak perlu membuat deklarasi berulang2
+//   let mahasiswa = Object.create(methodMahasiwa);
 
-function Mahasiswa(nama, energi) {
-  // * Penggunaan Object.create agar tidak perlu membuat deklarasi berulang2
-  let mahasiswa = Object.create(methodMahasiwa);
+//   //   let mahasiswa = {};
+//   mahasiswa.nama = nama;
+//   mahasiswa.energi = energi;
+//   //   mahasiswa.makan = methodMahasiwa.makan;
+//   //   mahasiswa.main = methodMahasiwa.main;
+//   //   mahasiswa.tidur = methodMahasiwa.tidur;
 
-  //   let mahasiswa = {};
-  mahasiswa.nama = nama;
-  mahasiswa.energi = energi;
-  //   mahasiswa.makan = methodMahasiwa.makan;
-  //   mahasiswa.main = methodMahasiwa.main;
-  //   mahasiswa.tidur = methodMahasiwa.tidur;
+//   return mahasiswa;
+// }
 
-  return mahasiswa;
-}
-
-let abu = Mahasiswa("Abu Abdirohman", 10);
-let hanan = Mahasiswa("Azati Hanani", 10);
+// let abu = Mahasiswa("Abu Abdirohman", 10);
+// let hanan = Mahasiswa("Azati Hanani", 10);
 
 // TODO 3. Constructor Function
 // ? Menggunakan keyword new
@@ -100,5 +99,57 @@ let hanan = Mahasiswa("Azati Hanani", 10);
 // }
 
 // let hanan = new Mahasiwa("Azati Hanani", 10);
+
+// // * Materi 1.3. Prototype
+
+// function Mahasiswa(nama, energi) {
+//   //   let this = {Object.create(Mahasiswa.prototype)}; // ? Sebenarnya isinya ada ini
+//   this.nama = nama;
+//   this.energi = energi;
+//   //   return this; // ? Dan ada ini juga
+
+//   Mahasiswa.prototype.makan = function (porsi) {
+//     this.energi += porsi;
+//     return `Halo ${this.nama}, selamat makan!`;
+//   };
+
+//   Mahasiswa.prototype.main = function (jam) {
+//     this.energi -= jam;
+//     return `Halo ${this.nama}, selamat main!`;
+//   };
+
+//   Mahasiswa.prototype.main = function (jam) {
+//     this.energi += jam * 2;
+//     return `Halo ${this.nama}, selamat tidur!`;
+//   };
+// }
+
+// let hanan = new Mahasiswa("Azati Hanani", 10);
+
+// * Versi Class
+class Mahasiswa {
+  constructor(nama, energi) {
+    this.nama = nama;
+    this.energi = energi;
+  }
+
+  makan(porsi) {
+    this.energi += porsi;
+    return `Halo ${this.nama}, selamat makan!`;
+  }
+
+  main(jam) {
+    this.energi -= jam;
+    return `Halo ${this.nama}, selamat main!`;
+  }
+
+  tidur(jam) {
+    this.energi += jam * 2;
+    return `Halo ${this.nama}, selamat tidur!`;
+  }
+}
+
+let abu = new Mahasiswa("Abu Abdirohman", 10);
+let hanan = new Mahasiswa("Azati Hanani", 10);
 
 // TODO 4. Object.create
