abb0020c54376dae7f6ddd34d3357b802d62bc9d
abuabdirohman4 <abuabdirohman4@gmail.com>
2022-07-13 15:44:15 +0700

    Materi 1.3 Prototype

---
+++ 01-Prototype/03-Prototype/03-array.js
@@ -0,0 +1,17 @@
+let angka = [] //array
+
+let angka1 = new Array() // di belakang layar
+// function Array() {
+//     let this = Object.create(Array.prototype)
+//     return this
+// }
+
+// Sehingga array memiliki banyak method (prototype dari object array)
+let angka2 = [1, 2, 3]
+console.log(angka2.reverse())
+console.log(angka2.sort())
+
+// Array.prototype (untuk melihat semua prototype yang dimiliki array)
+// atau object, number, dll
+// Object.prototype 
+// Number.prototype
